7a2b68b57dcd948a3ac474689277db2a556d52d3
Lostage <lostage30030@gmail.com>
2020-12-01 08:36:59 +0100

    first git

---
+++ Assets/_Scripts/FirstPersonController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 4f;
+    [SerializeField] float walkSpeed = 6f;
+    [SerializeField] float gravity = -9f;
+    [SerializeField, Range(0.0f, 0.5f)] float moveSmooth = 0.3f;
+    [SerializeField, Range(0.0f, 0.5f)] float mouseSmooth = 0.03f;
+    [SerializeField] bool lockCursor = true;
+
+    float cameraPitch = 0.0f;
+    float downVelocity = 0.0f;
+
+    CharacterController characterController = null;
+
+    Vector2 currentDirection = Vector2.zero;
+    Vector2 currentDirectionVelocity = Vector2.zero;
+    Vector2 currentMouseDelta = Vector2.zero;
+    Vector2 currentMouseDeltaVelocity = Vector2.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void Update()
+    {
+        FirstPersonView();
+        Movement();
+    }
+
+    void FirstPersonView()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmooth);
+
+        cameraPitch -= currentMouseDelta.y * mouseSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+    }
+
+    void Movement()
+    {
+        Vector2 targetDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDirection.Normalize();
+
+        currentDirection = Vector2.SmoothDamp(currentDirection, targetDirection, ref currentDirectionVelocity, moveSmooth);
+
+        if (characterController.isGrounded)
+        {
+            downVelocity = 0.0f;
+        }
+        downVelocity += gravity * Time.deltaTime;
+
+        Vector3 velocity = (transform.forward * currentDirection.y + transform.right * currentDirection.x) * walkSpeed + Vector3.up * downVelocity;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
